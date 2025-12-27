f4becf096f94bf9e50730885ac2c08a4a4e7a170
Luca Gonzalez <luca.gonzalezp@gmail.com>
2025-12-27 18:14:56 +0100

    Adds base path correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 const site = process.env.SITE_URL || "https://example.com";
-const base = process.env.BASE_PATH || "/";
+const base = process.env.BASE_PATH || "/es";
 
 export default defineConfig({
   site,
