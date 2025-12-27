9756c6e3edf4a69b00e3244bdf811cabade70a5c
Luca Gonzalez <luca.gonzalezp@gmail.com>
2025-12-27 18:17:39 +0100

    chore: Update default base path to `/`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 const site = process.env.SITE_URL || "https://example.com";
-const base = process.env.BASE_PATH || "/es";
+const base = process.env.BASE_PATH || "/";
 
 export default defineConfig({
   site,
